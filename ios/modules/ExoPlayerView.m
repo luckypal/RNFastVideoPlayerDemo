d3585e79034d9b1eaef8cc743f66e1ba72ac2b65
luckypal <bigluckypal@gmail.com>
2021-02-16 22:34:04 -0600

    Make video player in native side

---
+++ ios/modules/ExoPlayerView.m
@@ -0,0 +1,20 @@
+//
+//  ExoPlayerView.m
+//  MyDemo
+//
+//  Created by Lucky on 2/16/21.
+//
+
+#import "ExoPlayerView.h"
+
+@implementation ExoPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ ios/modules/ExoPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  ExoPlayerView.swift
+//  MyDemo
+//
+//  Created by Lucky on 2/16/21.
+//
+
+import UIKit
+
+class ExoPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
